8d8e27b310a2f2ccefa8e3ed50416a5a45624572
awadi99 <adityawaghmare9990@gmail.com>
2026-01-13 14:54:38 +0530

    next Static and Dynamic Page

--- src/app/rendering/dynamic/page.jsx
+++ src/app/rendering/dynamic/page.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { db } from "@/app/doctordata/config/db";
 
-export default function page() {
+import React from 'react'
+const dynamic = 'force-dynamic';
+
+const Dynamic = async () => {
+  const [doctors] = await db.execute("select * from doctors");
+  console.log(doctors);
+
   return (
-    <div> </div>
+    <>
+      <h1 className="font-medium text-4xl text-center">Dynamic Page Doctors data</h1>
+      <div className="p-10">
+        <ul>
+          {
+            doctors.map((doctors) => (
+              <li className="list-disc list-inside p-1" key={doctors.doctor_id}>Doctor Name =: {doctors.doctor_name}</li>
+            ))
+          }
+        </ul>
+      </div>
+    </>
   )
 }
+
+
+export default Dynamic;
--- src/app/rendering/static/page.jsx
+++ src/app/rendering/static/page.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { db } from "@/app/doctordata/config/db";
 
-export default function page() {
+const StaticPage = async () =>{
+  const [doctors] = await db.execute("select * from doctors");
+  console.log(doctors);
+
   return (
-    <div>page</div>
+    <>
+    <div>
+      <h1 className="text-center font-medium text-3xl">Static Page Doctor Name </h1>
+      <ul className="p-10 list-disc list-inside">
+        {
+          doctors.map((doctors)=>{
+            return <li key={doctors.doctor_id} >
+                {doctors.doctor_name}
+            </li>
+          })
+        }
+      </ul>
+    </div>
+    </>
   )
 }
+
+export default StaticPage;
