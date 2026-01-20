98b2606b91253bbc4747ba5ac3e59cc1b0cd023d
awadi99 <adityawaghmare9990@gmail.com>
2026-01-20 15:13:58 +0530

    REDIRECT NEXT.JS

--- src/app/(user)/contact/contact.action.jsx
+++ src/app/(user)/contact/contact.action.jsx
@@ -1,15 +1,21 @@
 "use server"
 
 import { db } from "@/app/doctordata/config/db"
+import { redirect } from "next/navigation";
 
 
 const ContactAction = async(fromData)=>{
-    
-
+    try{
     const {name,email,message} = Object.fromEntries(fromData.entries());
 
     const res = await db.execute("INSERT INTO contact (name,email,message) VALUES (?, ?, ?)",[name,email,message]);
+    redirect("/");
+    }
+    catch (err){
+        if(err.message=="NEXT_REDIRECT")throw err
+        console.error(err);
 
+    }
 
 }
 
