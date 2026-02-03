f17e5926e2e6b5fb8d69960339c2204aedc783df
awadi99 <adityawaghmare9990@gmail.com>
2026-02-03 14:00:43 +0530

    toast

---
+++ src/app/sonner/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SonnerDemo } from './SonnerDemo'
+
+export default function page() {
+  return (
+    <div className='flex flex-col items-center translate-y-80 '>
+    <SonnerDemo/>
+    </div>
+  )
+}
